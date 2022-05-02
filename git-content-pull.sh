2aa68016bd2af09a69a28b32cfbb08b1e8096733
n0_ware <eddie@hartbeat.io>
2022-05-02 14:47:45 -0500

    Test

--- git-content-pull.sh
+++ git-content-pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -u n0-ware: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 https://api.github.com/repos/n0-ware/course-notes/contents/Tools%2C%20Binaries%2C%20and%20Programs/CLI%20Utilities/Fundamental%20Linux/sed.md
+curl -u n0-ware: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 https://api.github.com/repos/n0-ware/course-notes/contents/Tools%2C%20Binaries%2C%20and%20Programs/CLI%20Utilities/Fundamental%20Linux/sed.md
 
 
